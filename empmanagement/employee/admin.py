8ea4f4935f43aa38a584451e091f1b78a3f94b5b
Patel Hadaisa <patelhadaisa@gmail.com>
2025-03-04 20:53:09 +0300

    Initial commit

---
+++ empmanagement/employee/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from employee.models import Employee,Attendance,Notice, Requests
+
+# Customize Admin Titles
+admin.site.site_header = "Employee Tracking System Admin"
+admin.site.site_title = "ETS Admin Panel"
+admin.site.index_title = "Welcome to the Employee Tracking System Dashboard"
+
+
+# Register your models here.
+admin.site.register(Employee)
+admin.site.register(Attendance)
+admin.site.register(Notice)
+admin.site.register(Requests)
